40802d2c1b9bd2d8c5b5fd1eeb47cc34719252b5
pohlinwei <linwei@Lins-MacBook-Pro.local>
2020-04-21 14:41:22 +0800

    Move cnn into package

--- cnnModel/tcnn/TcnnWithFC.py
+++ cnnModel/tcnn/TcnnWithFC.py
@@ -1,22 +1,22 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
 
-from Tcnn import Tcnn
+from tcnn.Tcnn import Tcnn
 
 """ 
 Uses base_model for transfer learning.
-Builds model with removal of fully-connected layers. 
+Builds model without removal of hidden layers. Only output layer is removed.
 Two additional adaptation layers are added. 
 """ 
-class TcnnWithoutFC(Tcnn):
+class TcnnWithFC(Tcnn):
     def __init__(self, base_model):
         super().__init__(base_model)
 
     def build_model(self):
         # freeze layers
         self.base_model.trainable = False
-        # obtain layers
-        feature_list = [layer.output for layer in self.base_model.layers[:-3]]
+        # remove output layer
+        feature_list = [layer.output for layer in self.base_model.layers[:-1]]
         # add adaptation layers
         feature_fc = feature_list[len(feature_list) - 1]
         fc = Dense(2048, activation='relu')(feature_fc)
